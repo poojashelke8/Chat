a3c0f2e54f4ca63672b4068eab26a404a0df668c
poojashelke8 <poojabshelke0807@gmail.com>
2026-05-30 20:48:19 +0530

    added pubsub (subscribe-publish)

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI,WebSocket,WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from redis import Redis
 import json
-# from app.websocket.manager import ConnectionManager
+from app.websocket.manager import ConnectionManager
+from app.pubsub import start_pubsub
 
 app = FastAPI()
 
@@ -12,6 +13,11 @@ redis_client = Redis(
     port=6379,
     decode_responses=True
     )
+
+manager = ConnectionManager()
+
+
+start_pubsub(redis_client,manager)
 
 
 @app.post("/task")
@@ -37,81 +43,84 @@ def delete_task():
         "task": task
     }
 
-# manager = ConnectionManager()
 
-# html = """
-# <!DOCTYPE html>
-# <html>
-#     <head>
-#         <title>FastAPI WebSocket Test</title>
-#     </head>
-#     <body>
-#         <h1>WebSocket Test Client</h1>
-#         <div id="messages"></div>
-#         <input type="text" id="messageText" placeholder="Type a message...">
-#         <button onclick="sendMessage()">Send</button>
-#         <script>
-#         const username = prompt("Enter username");
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI WebSocket Test</title>
+    </head>
+    <body>
+        <h1>WebSocket Test Client</h1>
+        <div id="messages"></div>
+        <input type="text" id="messageText" placeholder="Type a message...">
+        <button onclick="sendMessage()">Send</button>
+        <script>
+        const username = prompt("Enter username");
 
-# const ws = new WebSocket(
-#     `ws://127.0.0.1:8002/ws/${username}`
-# );
+const ws = new WebSocket(
+    `ws://127.0.0.1:8002/ws/${username}`
+);
             
-# ws.onopen = function () {
-#     console.log("WebSocket connected");
-# };
+ws.onopen = function () {
+    console.log("WebSocket connected");
+};
 
-# ws.onerror = function (err) {
-#     console.log("WebSocket error:", err);
-# };
+ws.onerror = function (err) {
+    console.log("WebSocket error:", err);
+};
 
-# ws.onclose = function () {
-#     console.log("WebSocket closed");
-# };
+ws.onclose = function () {
+    console.log("WebSocket closed");
+};
 
-# ws.onmessage = function(event) {
-#     var messages = document.getElementById('messages');
-#     var message = document.createElement('div');
-#     message.textContent = event.data;
-#     messages.appendChild(message);
-# };
+ws.onmessage = function(event) {
+    var messages = document.getElementById('messages');
+    var message = document.createElement('div');
+    message.textContent = event.data;
+    messages.appendChild(message);
+};
 
-# function sendMessage() {
+function sendMessage() {
 
-#     const input = document.getElementById("messageText");
+    const input = document.getElementById("messageText");
 
-#     ws.send(input.value);
+    ws.send(input.value);
 
-#     input.value = "";
-# }
-#         </script>
-#     </body>
-# </html>
-# """
-# print("MAIN FILE LOADED")
+    input.value = "";
+}
+        </script>
+    </body>
+</html>
+"""
+print("MAIN FILE LOADED")
 
-# @app.get("/")
-# async def home():
+@app.get("/")
+async def home():
     
-#     return HTMLResponse(html)
+    return HTMLResponse(html)
 
-# @app.websocket("/ws/{username}")
-# async def websocket_endpoint(websocket: WebSocket,username:str):
+@app.websocket("/ws/{username}")
+async def websocket_endpoint(websocket: WebSocket,username:str):
 
-#     await manager.connect(websocket,username)
+    await manager.connect(websocket,username)
 
-#     try:
-#         await manager.broadcast(f"{username} joined chat")
+    try:
+        await manager.broadcast(f"{username} joined chat")
 
-#         while True:
+        while True:
 
-#             data = await websocket.receive_text()
+            data = await websocket.receive_text()
+            redis_client.publish(
+                "chat_room",
+                f"{username} : {data}"
+            )
 
-#             await manager.broadcast(f"{username}: {data}")
+            # await manager.broadcast(f"{username}: {data}")
 
-#     except WebSocketDisconnect:
+    except WebSocketDisconnect:
 
-#         manager.disconnect(websocket)
+        manager.disconnect(websocket)
 
-#         await manager.broadcast(f"{username} left chat")
+        await manager.broadcast(f"{username} left chat")
     
